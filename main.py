bf7a1594182ee496675b76db2d879803a4e04cdc
jakebob4518 <74426060+jakebob4518@users.noreply.github.com>
2021-08-04 12:47:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+@app.route("/main")
+def home_page():
+    return render_template("home_page.html")
+
+
+@app.route("/market")
+@app.route("/shop")
+def market_page():
+    return render_template("market_page.html")
